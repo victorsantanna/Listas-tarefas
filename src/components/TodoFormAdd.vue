db36503eec5e80925f84e1d1a1e50855dc6a31fe
victorsantanna <victor.esef@hotmail.com>
2024-04-29 09:24:29 -0300

    feat: criação do TodoFormAdd

---
+++ src/components/TodoFormAdd.vue
@@ -0,0 +1,18 @@
+<template>
+     <!-- Todo form -->
+        <form class="flex items-center px-4 bg-gray-900 h-15 rounded-sm border-l-2 border-blue-400 mb-3">
+              <input
+                  placeholder="Adicione um novo item ..."
+                  type="text"
+                  class="bg-gray-900 placeholder-gray-500 text-gray-500 font-light focus:outline-none block w-full appearance-none leading-normal py-3 pr-3"
+              >
+
+              <button
+                  class="text-blue-400 text-xs font-semibold focus:outline-none"
+                  type="submit"
+              >
+                  ADICIONAR
+              </button>
+        </form>
+        <!--/ Todo form -->
+</template>
